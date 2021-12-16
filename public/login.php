6cfdae43140498e527acf5c210904cf84ec6ce6a
chrs-m <mister.ch@gmail.com>
2021-12-16 16:15:36 +0100

    New login page

--- public/login.php
+++ public/login.php
@@ -1,26 +1,35 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
-    <h1>Login</h1>
-
-    <form action="app/users/login.php" method="post">
-        <div class="mb-3">
-            <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" required>
-            <small class="form-text">Please provide the your email address.</small>
-        </div>
-
-        <div class="mb-3">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text">Please provide the your password (passphrase).</small>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+<article class="flex justify-center">
+    <div class="flex mt-24 w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="app/users/login.php" method="post">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    E-mail
+                </label>
+                <input class="form-control shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" id="email" placeholder="your@email.com" required>
+                <small class="text-xs italic">Please provide the your email address.</small>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2 mt-4" for="password">
+                        Password
+                    </label>
+                    <input class="form-control shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" name="password" id="password" placeholder="******************" required>
+                    <small class="text-xs italic">Please provide the your password (passphrase).</small>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Sign In
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Forgot Password?
+                    </a>
+                </div>
+        </form>
+        <a class="flex justify-center mt-3 text-blue-300" href="/signup.php">No account? Sign up here!</a>
+    </div>
 </article>
 
-<a href="/signup.php">No account? Sign up here!</a>
+
 
 <?php require __DIR__ . '/views/footer.php'; ?>
