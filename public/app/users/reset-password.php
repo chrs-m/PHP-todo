6e07bb637b5b0ed0c03e2f738b656f91dda91242
antmar0417 <tonyboy@live.se>
2022-01-23 21:40:28 +0100

    working on password

--- public/app/users/reset-password.php
+++ public/app/users/reset-password.php
@@ -3,3 +3,39 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if (isset($_POST['email'])) {
+    $emailTo = $_POST['email'];
+
+    try {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = "true";
+        $mail->SMTPSecure = "tls";
+        $mail->Port = "587";
+        $mail->Username = "antmar0417@skola.goteborg.se";
+        // Type a password here
+        $mail->Password = "AudiRS800+";
+        $mail->Subject = "Test Email 23/01";
+        $mail->setFrom("antmar0417@skola.goteborg.se");
+        $linkEmail = "http://localhost:8080/reset-password-link.php?email=" . $emailTo;
+        $mail->isHTML(true);
+        $mail->Body = "<h1>You requested a password reset! </h1>
+            Click on <a href='$linkEmail'>this link</a> to reset the password";
+        // Reciever
+        $mail->addAddress($emailTo);
+        $mail->Send();
+        echo "Reset password link has been sent to your email!";
+
+        $mail->smtpClose();
+    } catch (Exception $e) {
+        echo 'Error: ', $mail->ErrorInfo;
+    }
+    redirect('/index.php');
+}
---
+++ public/app/users/update/password-via-link.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../autoload.php';
+
+
+// HERE WE UPDATE THE PASSWORD IN THE DATABASE
+
+if ((isset($_POST['new-password'], $_POST['email']) && $_POST['new-password'] === $_POST['confirm-new-password'])) {
+
+    $newPassword = password_hash($_POST['new-password'], PASSWORD_DEFAULT);
+    // $confirmNewPassword = password_hash($_POST['confirm-new-password'], PASSWORD_DEFAULT);
+
+    // $statement = $database->prepare('SELECT password FROM users WHERE id = :id AND username = :username');
+    // $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    // $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    // $statement->execute();
+    // $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $statement = $database->prepare('UPDATE users SET password = :password WHERE id = :id');
+    $statement->bindParam(':password', $confirmNewPassword, PDO::PARAM_STR);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $_SESSION['message'] = 'Your password has been changed!';
+    redirect('/index.php');
+}
